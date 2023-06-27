403f6d116e49df2502d3f91823d40ac29c702353
rijulg <rijug06@gmail.com>
2023-06-26 20:48:10 -0700

    working user auth (login/register/logout)

--- app/src/main/java/com/rijulg/homefarm/LoginFragment.kt
+++ app/src/main/java/com/rijulg/homefarm/LoginFragment.kt
@@ -2,12 +2,19 @@ package com.rijulg.homefarm
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.rijulg.homefarm.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -18,6 +25,13 @@ class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,6 +41,14 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(activity, AppActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,10 +59,33 @@ class LoginFragment : Fragment() {
         }
 
         binding.loginButton.setOnClickListener {
-            val intent = Intent(activity, AppActivity::class.java)
-            startActivity(intent)
+            loginUser()
         }
 
+    }
+
+    private fun loginUser() {
+        val email = binding.emailField.text.toString()
+        val password = binding.passwordField.text.toString()
+
+        if (TextUtils.isEmpty(email)){
+            binding.emailField.error = "Email cannot be empty"
+            binding.emailField.requestFocus()
+        } else if (TextUtils.isEmpty(password)) {
+            binding.passwordField.error = "Password cannot be empty"
+            binding.passwordField.requestFocus()
+        } else {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()){task: Task<AuthResult> ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(requireActivity(), "User signed in successfully", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(requireActivity(), AppActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(requireActivity(), "Sign in error:" + task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/rijulg/homefarm/RegisterFragment.kt
+++ app/src/main/java/com/rijulg/homefarm/RegisterFragment.kt
@@ -1,15 +1,31 @@
 package com.rijulg.homefarm
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.rijulg.homefarm.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +34,39 @@ class RegisterFragment : Fragment() {
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.doneRegister.setOnClickListener {
+            createUser()
+        }
+
+    }
+
+    private fun createUser() {
+        val email = binding.registerEmail.text.toString()
+        val password = binding.registerPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)){
+            binding.registerEmail.error = "Email cannot be empty"
+            binding.registerEmail.requestFocus()
+        } else if (TextUtils.isEmpty(password)) {
+            binding.registerPassword.error = "Password cannot be empty"
+            binding.registerPassword.requestFocus()
+        } else {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()) { task: Task<AuthResult> ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(requireActivity(), "User registered successfully", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(requireActivity(), AppActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(requireActivity(), "Registration error:" + task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 
     override fun onDestroyView() {
