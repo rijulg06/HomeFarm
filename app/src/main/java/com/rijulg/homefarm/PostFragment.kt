b5a2ecbd54f19083cc8f51a4337251159b76e124
rijulg <rijug06@gmail.com>
2023-07-10 17:11:46 -0700

    basic post UI implemented

--- app/src/main/java/com/rijulg/homefarm/PostFragment.kt
+++ app/src/main/java/com/rijulg/homefarm/PostFragment.kt
@@ -1,6 +1,32 @@
 package com.rijulg.homefarm
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.rijulg.homefarm.databinding.FragmentPostBinding
 
 class PostFragment : Fragment() {
+
+    // View binding variables
+    private var _binding: FragmentPostBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // View binding variables initialized
+        _binding = FragmentPostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
