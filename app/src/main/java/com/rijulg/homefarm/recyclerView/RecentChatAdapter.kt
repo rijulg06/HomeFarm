d1aef6c3396e49bdf4eba7fb97302ef03133a339
rijulg <rijug06@gmail.com>
2023-07-25 15:43:39 -0700

    working item clicked in inbox frag

--- app/src/main/java/com/rijulg/homefarm/recyclerView/RecentChatAdapter.kt
+++ app/src/main/java/com/rijulg/homefarm/recyclerView/RecentChatAdapter.kt
@@ -2,10 +2,13 @@ package com.rijulg.homefarm.recyclerView
 
 import android.content.Context
 import android.content.res.Resources
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
@@ -13,6 +16,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.rijulg.homefarm.AppActivity
+import com.rijulg.homefarm.ChatFragment
 import com.rijulg.homefarm.R
 import com.rijulg.homefarm.models.Room
 import com.rijulg.homefarm.models.User
@@ -50,14 +55,50 @@ class RecentChatAdapter(options: FirestoreRecyclerOptions<Room>) :
 
         holder.recentTime.text = room.lastMessage?.sentAt.toString()
 
+        holder.itemView.setOnClickListener { v ->
+
+            if (room.fromUid == Firebase.auth.currentUser?.uid) {
+                FirebaseFirestore.getInstance().collection("users").document(room.toUid).get()
+                    .addOnSuccessListener { userSnapshot ->
+                        val toUser = userSnapshot.toObject(User::class.java)
+
+                        val bundle = Bundle()
+                        bundle.putSerializable("toUser", toUser)
+
+                        val activity = v!!.context as AppActivity
+                        val chatFragment = ChatFragment()
+                        chatFragment.arguments = bundle
+                        activity.supportFragmentManager.beginTransaction()
+                            .replace(R.id.frame_layout, chatFragment).commit()
+
+                    }
+            } else if (room.toUid == Firebase.auth.currentUser?.uid) {
+                FirebaseFirestore.getInstance().collection("users").document(room.fromUid).get()
+                    .addOnSuccessListener { userSnapshot ->
+                        val toUser = userSnapshot.toObject(User::class.java)
+
+                        val bundle = Bundle()
+                        bundle.putSerializable("toUser", toUser)
+
+                        val activity = v!!.context as AppActivity
+                        val chatFragment = ChatFragment()
+                        chatFragment.arguments = bundle
+                        activity.supportFragmentManager.beginTransaction()
+                            .replace(R.id.frame_layout, chatFragment).commit()
+
+                    }
+            }
+
+        }
+
     }
 
 
     class RoomAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-            val recentUser: TextView = itemView.findViewById(R.id.recentUser)
-            val recentMessage: TextView = itemView.findViewById(R.id.recentMessage)
-            val recentTime: TextView = itemView.findViewById(R.id.recentTime)
+        val recentUser: TextView = itemView.findViewById(R.id.recentUser)
+        val recentMessage: TextView = itemView.findViewById(R.id.recentMessage)
+        val recentTime: TextView = itemView.findViewById(R.id.recentTime)
 
     }
 }
